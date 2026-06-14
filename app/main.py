7cc1d1af4c97e48ef108c590cc9895210af72251
vineeth <ervineethpv137@gmail.com>
2026-06-14 07:42:16 +0000

    Initial commit: Python backend app

---
+++ app/main.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({"status": "success", "message": "Python Backend is running successfully!"})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
